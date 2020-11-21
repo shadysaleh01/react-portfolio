1346342e0a6e9172952326a1d89cf615d5117485
Shady Coder <shadysaleh01@gmail.com>
2020-11-20 21:11:31 -0800

    menu icon with funtion to switch the icon when user click on it

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
    const [click, setClick] = useState(false)
-
+   const handleClick = () => setClick(!click)
 
    return (
       <div>
@@ -12,7 +12,9 @@ function Navbar() {
                <Link to="/" className="navbar-logo">
                   Shady Saleh <i className="fab fa-typo3" />
                </Link>
-
+               <div className="menu-icon" onClick={handleClick}>
+                  <i className={click ? "fas fa-times" : "fas fa-bars"} />
+               </div>
 
             </div>
          </nav>
