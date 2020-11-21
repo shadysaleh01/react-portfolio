38e470e7e94a817f13f238bdc08e52168f33c524
Shady Coder <shadysaleh01@gmail.com>
2020-11-21 01:47:03 -0800

    Added info about me and button to hero section

--- client/src/components/HeroSection/index.js
+++ client/src/components/HeroSection/index.js
@@ -5,6 +5,13 @@ function HeroSection() {
    return (
       <div className="hero-container">
          <video ser="/videos/video-2.mp4" autoPlay loop muted />
+         <h1>Hi, I'm Shady Saleh</h1>
+         <h3>I Am Web-Developer</h3>
+         <Button className="btns"
+            buttonStyle="btn--primary"
+            buttonSize="btn--large">
+            LET'S WORK TOGETHER <i className="far fa-work" />
+         </Button>
       </div>
    )
 };
