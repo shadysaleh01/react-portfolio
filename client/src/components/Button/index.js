36d0a321c5b1e5556b122a84c6507eb3c590d649
Shady Coder <shadysaleh01@gmail.com>
2020-11-20 22:26:28 -0800

    created button function inside button component takes parameters

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -1,1 +1,12 @@
-import React from 'react'
+import React from 'react';
+import "./style.css";
+import { Link } from "react-router-dom";
+
+
+
+
+export const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
+
+
+
+}
