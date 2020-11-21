fea5a016709c8fef79a2125e984326d4b100dceb
Shady Coder <shadysaleh01@gmail.com>
2020-11-21 01:25:24 -0800

    function return container with video inside it in hero section

--- client/src/components/HeroSection/index.js
+++ client/src/components/HeroSection/index.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React from 'react';
+import "./style.css";
 
+function HeroSection() {
+   return (
+      <div className="hero-container">
+         <video ser="/videos/video-2.mp4" autoPlay loop muted />
+      </div>
+   )
+};
 
-
-export default HeroSection
+export default HeroSection;
 
