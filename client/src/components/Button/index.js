a1072326ac17d76001423d1a1064ccce849163ca
Shady Coder <shadysaleh01@gmail.com>
2020-11-20 22:34:42 -0800

    pass the style that needed on the button, also there is default value

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -3,9 +3,10 @@ import "./style.css";
 import { Link } from "react-router-dom";
 
 
-
+const STYLES = ["btn--primary", "btn--outline"];
 
 export const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
+   const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
 
 
 
