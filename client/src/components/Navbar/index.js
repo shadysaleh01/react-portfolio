e2b15b7bbaf5b31bc903805fc56d2c7fbcf0f4c6
Shady Coder <shadysaleh01@gmail.com>
2020-11-20 23:08:21 -0800

    function will setButton to false if window size less than 960 and true if more than 960

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -8,7 +8,13 @@ function Navbar() {
    const handleClick = () => setClick(!click);
    const closeMobileMenu = () => setClick(false);
 
-
+   const showButton = () => {
+      if (window.innerWidth <= 960) {
+         setButton(false);
+      } else {
+         setButton(true);
+      }
+   }
 
    return (
       <div>
