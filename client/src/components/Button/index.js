f505c2bc7d8e6f82c24f34e9f54ca3658719e589
Shady Coder <shadysaleh01@gmail.com>
2020-11-20 22:42:58 -0800

    return link that has button holds all prarameters

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -11,4 +11,15 @@ export const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => 
    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize :
       SIZES[0];
 
+   return (
+      <Link to="/" className="btn-mobile">
+         <button
+            className={`btn ${checkButtonStyle} ${checkButtonStyle}`}
+            onClick={onClick}
+            type={type}
+         >
+            {children}
+         </button>
+      </Link>
+   )
 }
