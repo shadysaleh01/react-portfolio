8c50f6e45fe09b18b93250858d0ab5d087d9df36
Shady Coder <shadysaleh01@gmail.com>
2020-11-20 21:19:52 -0800

    navbar home by clicking on it will close navbar and take user to home page

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,7 +4,7 @@ import { Link, NavLink } from 'react-router-dom'
 function Navbar() {
    const [click, setClick] = useState(false)
    const handleClick = () => setClick(!click)
-
+   const closeMobileMenu = () => setClick(false)
    return (
       <div>
          <nav className="navbar">
@@ -15,7 +15,14 @@ function Navbar() {
                <div className="menu-icon" onClick={handleClick}>
                   <i className={click ? "fas fa-times" : "fas fa-bars"} />
                </div>
+               <ul className={click ? "nav-menu active" : "nav-menu"}>
+                  <li className="nav-item">
+                     <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                        Home
+                     </Link>
 
+                  </li>
+               </ul>
             </div>
          </nav>
       </div>
