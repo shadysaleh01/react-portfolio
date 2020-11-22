4139c152dab4e86297d1222713ccd3934d2c6c5c
Shady Coder <shadysaleh01@gmail.com>
2020-11-21 22:14:10 -0800

    cteated li elements in card item component to use it in card component

--- client/src/components/CardItem/index.js
+++ client/src/components/CardItem/index.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 
 function CardItem() {
    return (
       <div>
-
+         <li className='cards__item'>
+            <Link className="cards__item__link">
+               <figure className="cards__item__pic-wrap">
+                  <img src="/" alt="" className="cards__item__img" />
+               </figure>
+               <div className="cards__item__info">
+                  <h5 className="cards__item__text" />
+               </div>
+            </Link>
+         </li>
       </div>
-   )
+   );
 }
 
-export default CardItem
+export default CardItem;
