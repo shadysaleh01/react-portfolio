e68e6a5b889e6dbe9ff60f58dc0ad0f0534994b8
Shady Saleh <shadysaleh@Shadys-MBP.attlocal.net>
2021-02-02 19:26:27 -0800

    Deployed to github

---
+++ build/precache-manifest.371c5b5f35d5769611b612b11af38258.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bac0a5e0fb6e2ed3887a21db2b6e725",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dea608993db90c37cc3",
+    "url": "/static/css/2.e833e581.chunk.css"
+  },
+  {
+    "revision": "4f4a6a6d9f7534592841",
+    "url": "/static/css/main.10f65675.chunk.css"
+  },
+  {
+    "revision": "9dea608993db90c37cc3",
+    "url": "/static/js/2.aca93644.chunk.js"
+  },
+  {
+    "revision": "a8c4affbdc36a243c05e0dd945fd517c",
+    "url": "/static/js/2.aca93644.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4a6a6d9f7534592841",
+    "url": "/static/js/main.81387818.chunk.js"
+  },
+  {
+    "revision": "d08766e1dd2bf4f6c209",
+    "url": "/static/js/runtime-main.992eac29.js"
+  },
+  {
+    "revision": "2fc6a5be9a8df9af2e8eb77dbc652346",
+    "url": "/static/media/MyResume.2fc6a5be.pdf"
+  },
+  {
+    "revision": "16ae8dfe2cbbcf0564d1088956c61c24",
+    "url": "/static/media/barber-img.16ae8dfe.png"
+  },
+  {
+    "revision": "a2d57174222afae2f9236abf3b3d5482",
+    "url": "/static/media/burger.a2d57174.png"
+  },
+  {
+    "revision": "8efb2ccd21aad61f6f7f4da7c7e04988",
+    "url": "/static/media/coffee.8efb2ccd.jpg"
+  },
+  {
+    "revision": "54cbee4dd8fc6f7863375cd3d88bb6f9",
+    "url": "/static/media/employee-engin.54cbee4d.PNG"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "e77e002e52ef5b3544e615f382a53ee6",
+    "url": "/static/media/portfolio.e77e002e.jpg"
+  },
+  {
+    "revision": "5b620e628e53f0a7c6515d3ee8915a1f",
+    "url": "/static/media/scheduler.5b620e62.png"
+  },
+  {
+    "revision": "aaf3d7d4f9645644b21c7de001c6752e",
+    "url": "/static/media/trivia.aaf3d7d4.jpg"
+  }
+]);
