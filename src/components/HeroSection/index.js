3d72cc9463935acdd7f18080724094ea7816dcf4
Shady Saleh <shadysaleh01.com>
2021-04-09 21:25:21 -0700

    Updated hero section

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { Button } from '../Button';
+// import Button from "react-bootstrap/Button"
 import Aos from "aos";
 import "aos/dist/aos.css";
 import "./style.css";
 import "../../App.css"
+import ScrollspyNav from 'react-scrollspy-nav';
+
 // import video from "../../videos/video-1.mp4"
 
 
@@ -20,11 +23,14 @@ function HeroSection() {
          </h1>
          <h2 className="animated fadeInUp delay-05s" data-aos="zoom-out-down">I'm a Web-Developer</h2>
          <div data-aos="flip-right">
+         <ScrollspyNav scrollTargetIds={["about","home"]} >
             <Button className="btns"
-               buttonStyle="btn--primary"
-               buttonSize="btn--large">
+               buttonStyle="btn--outline"
+               buttonSize="btn--large"
+               href="#about">
                LET'S WORK TOGETHER <i className="far fa-work" />
             </Button>
+         </ScrollspyNav>
          </div>
       </div>
    )
