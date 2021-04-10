cfec24038f403be5c6f5987e67c3f17738f82c04
Shady Saleh <shadysaleh01.com>
2021-04-09 21:25:48 -0700

    Updates

---
+++ precache-manifest.b9bd201aad638c4bb0da89988ea526a3.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61f05395f9241613bd7700dc79ccd31",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "3df8edf88b5de63710ac",
+    "url": "/react-portfolio/static/css/2.e833e581.chunk.css"
+  },
+  {
+    "revision": "8d144e0c0061ce1f4e9d",
+    "url": "/react-portfolio/static/css/main.44e878bf.chunk.css"
+  },
+  {
+    "revision": "3df8edf88b5de63710ac",
+    "url": "/react-portfolio/static/js/2.b95937e6.chunk.js"
+  },
+  {
+    "revision": "a8c4affbdc36a243c05e0dd945fd517c",
+    "url": "/react-portfolio/static/js/2.b95937e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d144e0c0061ce1f4e9d",
+    "url": "/react-portfolio/static/js/main.6f4845c1.chunk.js"
+  },
+  {
+    "revision": "abd8dc3e5ba9a474aea2",
+    "url": "/react-portfolio/static/js/runtime-main.fcb30b24.js"
+  },
+  {
+    "revision": "111b3af254d39c21866b3659d45f8514",
+    "url": "/react-portfolio/static/media/ShadySaleh-Resume.111b3af2.pdf"
+  },
+  {
+    "revision": "16ae8dfe2cbbcf0564d1088956c61c24",
+    "url": "/react-portfolio/static/media/barber-img.16ae8dfe.png"
+  },
+  {
+    "revision": "a2d57174222afae2f9236abf3b3d5482",
+    "url": "/react-portfolio/static/media/burger.a2d57174.png"
+  },
+  {
+    "revision": "8efb2ccd21aad61f6f7f4da7c7e04988",
+    "url": "/react-portfolio/static/media/coffee.8efb2ccd.jpg"
+  },
+  {
+    "revision": "54cbee4dd8fc6f7863375cd3d88bb6f9",
+    "url": "/react-portfolio/static/media/employee-engin.54cbee4d.PNG"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/react-portfolio/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "369d2a847d6d7aa6ef24fc4b1a17204f",
+    "url": "/react-portfolio/static/media/img-home.369d2a84.jpg"
+  },
+  {
+    "revision": "e77e002e52ef5b3544e615f382a53ee6",
+    "url": "/react-portfolio/static/media/portfolio.e77e002e.jpg"
+  },
+  {
+    "revision": "5b620e628e53f0a7c6515d3ee8915a1f",
+    "url": "/react-portfolio/static/media/scheduler.5b620e62.png"
+  },
+  {
+    "revision": "aaf3d7d4f9645644b21c7de001c6752e",
+    "url": "/react-portfolio/static/media/trivia.aaf3d7d4.jpg"
+  },
+  {
+    "revision": "3cb2fa2c0a0b936a1d23bb3bf139a120",
+    "url": "/react-portfolio/static/media/video-2.3cb2fa2c.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.3b77a653eab670f501b760141bdb0249.js"
+  "/react-portfolio/precache-manifest.b9bd201aad638c4bb0da89988ea526a3.js"
 );
 
 self.addEventListener('message', (event) => {
